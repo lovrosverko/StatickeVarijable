a3f0451cb7660c2967bda48dc708f514147b51f7
Lovro Šverko <lovro.sverko@gmail.com>
2020-10-12 08:30:43 +0200

    Add files via upload

---
+++ statickeVarijable/statickeVarijable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statickeVarijable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa prvi = new PrvaKlasa("Pero", "Perić");
+            Console.WriteLine(prvi.ToString());
+
+            PrvaKlasa drugi = new PrvaKlasa("Ivica", "Ivić");
+            Console.WriteLine(drugi.ToString());
+
+            PrvaKlasa treci = new PrvaKlasa();
+            Console.WriteLine(treci.ToString());
+
+            treci.Ime = "Milorad";
+            treci.Prezime = "Milić";
+
+            Console.WriteLine(treci.ToString());
+   
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ statickeVarijable/statickeVarijable/PrvaKlasa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statickeVarijable
+{
+    class PrvaKlasa
+    {
+        string ime;
+        string prezime;
+        static int brojac;
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public PrvaKlasa()
+        {
+            brojac++;
+        }
+        public PrvaKlasa(string ime, string prezime)
+        {
+            brojac++;
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Redni broj: "+brojac+", "+ime+" " +prezime;
+            return ispis;
+        }
+
+    }
+}
